493bd297df2f3c3f64b7c5ee7d567bc09de16410
Emile Belanger <emile.belanger@gmail.com>
2021-07-31 13:23:42 +0100

    Update version

--- source/core/version.h
+++ source/core/version.h
@@ -41,7 +41,7 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "0.10.0-preview"
+#define VERSIONSTR "1.1.3"
 
 // The version as seen in the Windows resource
 #define RC_FILEVERSION 0,9,0,0
