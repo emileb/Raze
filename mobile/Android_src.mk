be19e160e1243ebc6ad5b55c9e8f6e0b9bcaa13b
Emile Belanger <emile.belanger@gmail.com>
2025-09-06 19:05:46 +0100

    Fix Mac build

--- mobile/Android_src.mk
+++ mobile/Android_src.mk
@@ -91,7 +91,7 @@ LOCAL_C_INCLUDES := \
  $(TOP_DIR)/AudioLibs_OpenTouch/openal/include/AL \
  $(TOP_DIR)/Clibs_OpenTouch/jpeg8d \
  $(TOP_DIR)/Clibs_OpenTouch \
- $(TOP_DIR)/Clibs_OpenTouch\raze \
+ $(TOP_DIR)/Clibs_OpenTouch/raze \
  $(TOP_DIR)/MobileTouchControls  \
  $(TOP_DIR)/ZMusic/include  \
  $(GZDOOM_TOP_PATH)/mobile/src/extrafiles  \
